933ac29f457f7fb216e346f7a835e179288c09e7
thlibers <thlibers@student.42lehavre.fr>
2026-06-10 12:34:09 +0200

    ex02 done

--- cpp03/ex02/sources/FragTrap.cpp
+++ cpp03/ex02/sources/FragTrap.cpp
@@ -1,2 +1,52 @@
 #include "../includes/FragTrap.hpp"
 
+FragTrap::FragTrap(std::string Name) : ClapTrap(Name), state(false)
+{
+	this->_hitPoints = 100;
+	this->_energyPoints = 100;
+	this->_attackDamage = 30;
+	std::cout << "FragTrap " << this->_name << " constructor called" << std::endl;
+}
+
+FragTrap::~FragTrap()
+{
+	std::cout << "FragTrap " << this->_name << " destructor called" << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap &cpy) : ClapTrap(cpy), state(cpy.state)
+{
+	std::cout << "FragTrap copy constructor called" << std::endl;
+}
+
+FragTrap &FragTrap::operator=(const FragTrap &cpy)
+{
+	if (this != &cpy)
+	{
+		ClapTrap::operator=(cpy);
+		this->state = cpy.state;
+	}
+	std::cout << "FragTrap " << this->_name << " assignment operator called" << std::endl;
+	return (*this);
+}
+
+//	Functions :
+
+void FragTrap::attack(const std::string& target)
+{
+	if (this->_energyPoints > 0 && this->_hitPoints > 0)
+	{
+		std::cout << "FragTrap " << this->_name << " attacks " << target << ", causing " << this->_attackDamage << " points of damage!" << std::endl;
+		this->_energyPoints--;
+	}
+}
+
+void FragTrap::highFivesGuys()
+{
+	if (this->state == false)
+	{
+		this->state = true;
+		std::cout << "ScavTrap " << this->_name << " high-fives request!" << std::endl;
+	}
+	else
+		std::cout << "ScavTrap " << this->_name << " high-fives is already done!" << std::endl;
+}
--- cpp03/ex02/sources/main.cpp
+++ cpp03/ex02/sources/main.cpp
@@ -1,9 +1,23 @@
 #include "../includes/ScavTrap.hpp"
+#include "../includes/FragTrap.hpp"
 
 int main()
 {
-	ScavTrap	a("yderridj");
-	ScavTrap	b("ytennah");
+	FragTrap	a("yderridj");
+	FragTrap	b("ytennah");
+	ScavTrap	c("jhauvill");
+	ScavTrap	d("aronnet");
+
+	c.attack("aronnet");
+	d.takeDamage(3);
+	d.beRepaired(2);
+
+	d.attack("jhauvill");
+	c.takeDamage(5);
+	c.beRepaired(4);
+
+	c.guardGate();
+	d.guardGate();
 
 	a.attack("ytennah");
 	b.takeDamage(3);
@@ -13,8 +27,8 @@ int main()
 	a.takeDamage(5);
 	a.beRepaired(4);
 
-	a.guardGate();
-	b.guardGate();
+	a.highFivesGuys();
+	b.highFivesGuys();
 
 	return (0);
 }
